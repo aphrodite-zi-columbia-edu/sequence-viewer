949faf1d46502a0bbecd50c82c90bf5bd1d1c444
Dave Ankin <alexanderankin@gmail.com>
2017-09-08 01:56:29 -0400

    need sleep to finish backend

--- public/javascripts/settings/index.js
+++ public/javascripts/settings/index.js
@@ -1,17 +1,51 @@
-var which = "csv";
+
+function makeWhichTab(tabs) {
+  var current = tabs[Object.keys(tabs).slice(0, 1)].value;  // first key
+
+  function get() { return current; }
+  function set(t) { return current = t; }
+
+  // on ready attach listeners to tabs
+  $(function () {
+    for (var tabid in tabs) {
+      $(tabid).on('click', function () {
+        // console.log
+        set(tabs[tabid].value);
+      })
+    }
+  })
+
+  return {
+    get, set
+  };
+}
+
+var whichBwSource = makeWhichTab({
+  '#folder-nav-tab': { value: 'folder' },
+  '#url-nav-tab':    { value: 'url' }
+});
+var whichSequenceSource = makeWhichTab({
+  '#csv-nav-tab':   { value: 'csv' },
+  '#mysql-nav-tab': { value: 'mysql' }
+});
+
+
 function upload(callback) {
-  // alert($('#configuration-preview').val());
+  console.log(formatFields($("#settings-form :input").serializeArray()));
+  var data = formatFields($("#settings-form :input").serializeArray());
+  // $.post('/api/configvalid', data, function (data, status, jQXHR) {
+    
+  //   callback();
+  // })
   callback();
 }
 
 $(function() { //shorthand document.ready function 
-  $('#csv-nav-tab').on('click', function() { which = "csv" });
-  $('#mysql-nav-tab').on('click', function() { which = "mysql" });
   $('#save').on('click', upload.bind(null, function(err, resp) {
-    alert("Returned to callback");
+    console.log("Returned to callback");
   }));
 
-  preview();
+  preview(true);
   $('#settings-form').on('submit', function(e) { //use on if jQuery 1.7+
     e.preventDefault();  //prevent form from submitting
     preview();
@@ -23,11 +57,14 @@ $(function() { //shorthand document.ready function
  * 
  * Depends on the form being #settings-form
  * Depends on the text being #configuration-preview
+ * 
+ * Validates and reports with modal
  */
-function preview() {
+function preview(novalidate) {
   var data = $("#settings-form :input").serializeArray();
+  var configuration = formatFields(data);
 
-  var stringified = JSON.stringify(formatFields(data), function (key, value) {
+  var stringified = JSON.stringify(configuration, function (key, value) {
     // if (!value) return null; return value;
     // if (!value) return console.log(value), null; return value;
     if (typeof value === 'undefined') return "undefined";
@@ -37,8 +74,49 @@ function preview() {
   }, 2);
 
   $('#configuration-preview').val(stringified);
+
+  // if (!novalidate) validate(configuration);
 }
 
+/**
+ * Performs validation on configuration and calls report with errors
+ * 
+ * @param configuration object  the configuration object being checked
+ * @param doModal       boolean whether or not to display errors in a modal
+ */
+function validate(configuration, doModal, callback) {
+  callback = typeof callback === 'function' ? callback : function() {};
+
+  $.post('/api/configvalid', configuration, function (data, status, jQXHR) {
+    reportValidity(data);
+  });
+}
+
+function reportValidity(data) {
+  if (!data.valid) {
+    $("#validation-errors .modal-body").empty();
+    (data.errors || []).forEach(function (error) {
+      $("#validation-errors .modal-body").append($('<p>' + error + '</p>'));
+    });
+    var warning = '<p class="text-warning"><small>You can\'t save an'
+      + 'incorrect configuration.</small></p>';
+    $("#validation-errors .modal-body").append($(warning));
+    $('#validation-errors').modal('show');
+  }
+
+  else {
+    $('#success-disappear').fadeIn();
+    setTimeout(function() {
+      $('#success-disappear').fadeOut();
+    }, 1500);
+  }
+}
+
+/**
+ * Formats the fields into a valid configuration schema
+ * 
+ * depends on whichSequenceSource closure, et al makeWhichTab closures.
+ */
 function formatFields(data) {
   var formOptions = data.reduce(function(obj, el) {
     obj[el.name] = el.value;
@@ -54,20 +132,29 @@ function formatFields(data) {
     backup: formOptions.optionbackup === "on" || false
   };
 
-  switch (which) {
+  switch (whichBwSource.get()) {
+    case 'url':
+      config.bigWigConfig.folder = formOptions.bwurl;
+      break;
+    case 'folder':
+      config.bigWigConfig.folder = formOptions.bwdir;
+      break;
+  }
+
+  switch (whichSequenceSource.get()) {
     case 'csv':
       config.sequenceConfig.csv = {
-        'path': config.csvpath,
-        'column': config.csvcolumn,
-        'index': config.csvuseindex === "on" || false
+        'path': formOptions.csvpath,
+        'column': formOptions.csvcolumn,
+        'index': formOptions.csvuseindex === "on" || false
       };
       break;
     case 'mysql':
       config.sequenceConfig.mysql = {
-        host: config.dbhost,
-        name: config.dbname,
-        user: config.dbuser,
-        pass: config.dbpass        
+        host: formOptions.dbhost,
+        name: formOptions.dbname,
+        user: formOptions.dbuser,
+        pass: formOptions.dbpass        
       };
       break;
   }
--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.json({ msg: 'respond with a resource'});
 });
 
+router.post('/configvalid', function (req, res, next) {
+  console.log(req.body);
+  var validationErrors = validate(Object.assign({}, req.body));
+  if (validationErrors)
+    return res.json({ errors: validationErrors });
+
+  res.json({ valid: true });
+});
+
 module.exports = router;
+
+function validate(config) {
+  console.log(config);
+  var errors = [];
+
+  var requiredKeys = ['sequenceConfig', 'bigWigConfig', 'basetracks'];
+  for (var i = 0; i < requiredKeys.length; i++)
+    if (!(config.hasOwnProperty(requiredKeys[i])))
+      errors.push("Does not have property " + requiredKeys[i]);
+
+  var sequenceConfigs = ['csv', 'mysql'];
+  var configs = Object.keys(config.sequenceConfig || {})
+    .map((key) => key.toLowerCase())
+    .filter((key) => sequenceConfigs.indexOf(key) > -1);
+
+  if (configs.length !== 1)
+    errors.push("Exactly one Sequence Configuration allowed");
+
+  if (config.sequenceConfig && config.sequenceConfig.csv)
+    if (!fs.existsSync(config.sequenceConfig.csv.file))
+      errors.push("csv file doesnt exist");
+
+  if (!Array.isArray(config.basetracks))
+    errors.push("basetracks must be an Array");
+
+  return errors.length ? errors : false;
+}
