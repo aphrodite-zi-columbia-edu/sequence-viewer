18035134b47b52f91e01cb8260d7043341adc95b
Dave Ankin <alexanderankin@gmail.com>
2017-09-11 11:54:35 -0400

    login stuff, some untested

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,83 @@
 var express = require('express');
 var router = express.Router();
 
+var state = require('../lib/state').state;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title:   'Sequence Viewer',
-    scripts: [/*'<script src="/javascripts/example"></script>',*/
-      '<script src="/components/jquery/dist/jquery.js"></script>',
-      '<script src="/components/bootstrap/dist/js/bootstrap.js"></script>',
-    ],
-    styles:  [/*'<link rel="stylesheet" href="/components/example.css">',*/,
-      '<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.css">',
-    ]
-  });
+  if (state.hasAuthSetup()) {
+    res.render('login', {
+      needsUsername: state.hasRoot(),
+      scripts: [/*'<script src="/javascripts/example"></script>',*/
+        '<script src="/components/jquery/dist/jquery.js"></script>',
+        '<script src="/components/bootstrap/dist/js/bootstrap.js"></script>',
+      ],
+      styles:  [/*'<link rel="stylesheet" href="/components/example.css">',*/,
+        '<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.css">',
+      ]
+    })
+  }
+
+  else {
+    res.render('initial-setup', {
+      scripts: [
+        '<script src="/components/jquery/dist/jquery.js"></script>',
+        '<script src="/components/bootstrap/dist/js/bootstrap.js"></script>',
+      ],
+      styles: ['<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.css">']
+    });
+  }
+  // res.render('index', {
+  //   title:   'Sequence Viewer',
+  //   scripts: [/*'<script src="/javascripts/example"></script>',*/
+  //     '<script src="/components/jquery/dist/jquery.js"></script>',
+  //     '<script src="/components/bootstrap/dist/js/bootstrap.js"></script>',
+  //   ],
+  //   styles:  [/*'<link rel="stylesheet" href="/components/example.css">',*/,
+  //     '<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.css">',
+  //   ]
+  // });
 });
+
+router.post('/initialsetup', function (req, res, next) {
+  if (!state.hasAuthSetup()) {
+    if (req.body.password) {
+      state.setSinglePassword(req.body.password);
+      res.redirect('/');
+    }
+
+    else
+      res.render('initial-setup', {
+        scripts: [
+          '<script src="/components/jquery/dist/jquery.js"></script>',
+          '<script src="/components/bootstrap/dist/js/bootstrap.js"></script>',
+        ],
+        styles: ['<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.css">']
+      });
+  }
+  else
+    return next(new Error("Can't set up the one time password twice."));
+})
+
+router.post('/', function (req, res, next) {
+  var authFn = state.getAuthFn();
+  if (typeof authFn !== 'function') return res.redirect('/');
+  authFn(req.body.username, req.body.password, function (valid) {
+    if (valid)
+      res.render('loggedin', {
+        scripts: [
+          '<script src="/components/jquery/dist/jquery.js"></script>',
+          '<script src="/components/bootstrap/dist/js/bootstrap.js"></script>',
+        ],
+        styles:  [
+          '<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.css">',
+        ]
+      });
+
+    else
+      res.redirect('/');
+  })
+})
 
 router.get('/settings', function(req, res, next) {
   res.render('settings', {
